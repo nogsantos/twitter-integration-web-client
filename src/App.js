a6243d3f40b5adc4eedd62b5e967c1a70f8daefe
Fabricio Nogueira <nogsantos@gmail.com>
2019-02-14 22:49:24 -0200

    feat(init): Initial commit
    Project setup

--- src/App.js
+++ src/App.js
@@ -1,28 +1,189 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import { withStyles } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+
+import { Menu } from './components/index';
+
+const drawerWidth = 240;
+
+const styles = theme => ({
+	root: {
+		display: 'flex'
+	},
+	appBar: {
+		zIndex: theme.zIndex.drawer + 1,
+		transition: theme.transitions.create(['width', 'margin'], {
+			easing: theme.transitions.easing.sharp,
+			duration: theme.transitions.duration.leavingScreen
+		})
+	},
+	appBarShift: {
+		marginLeft: drawerWidth,
+		width: `calc(100% - ${drawerWidth}px)`,
+		transition: theme.transitions.create(['width', 'margin'], {
+			easing: theme.transitions.easing.sharp,
+			duration: theme.transitions.duration.enteringScreen
+		})
+	},
+	menuButton: {
+		marginLeft: 12,
+		marginRight: 36
+	},
+	hide: {
+		display: 'none'
+	},
+	drawer: {
+		width: drawerWidth,
+		flexShrink: 0,
+		whiteSpace: 'nowrap'
+	},
+	drawerOpen: {
+		width: drawerWidth,
+		transition: theme.transitions.create('width', {
+			easing: theme.transitions.easing.sharp,
+			duration: theme.transitions.duration.enteringScreen
+		})
+	},
+	drawerClose: {
+		transition: theme.transitions.create('width', {
+			easing: theme.transitions.easing.sharp,
+			duration: theme.transitions.duration.leavingScreen
+		}),
+		overflowX: 'hidden',
+		width: theme.spacing.unit * 7 + 1,
+		[theme.breakpoints.up('sm')]: {
+			width: theme.spacing.unit * 9 + 1
+		}
+	},
+	paper: {
+		marginTop: 80,
+		padding: theme.spacing.unit * 2,
+		color: theme.palette.text.secondary
+	}
+});
+
+const AppContext = React.createContext({
+	handleDrawerClose: () => {}
+});
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+	state = {
+		open: false
+	};
+
+	handleDrawerOpen = () => {
+		this.setState({ open: true });
+	};
+
+	handleDrawerClose = () => {
+		this.setState({ open: false });
+	};
+
+	render() {
+		const { handleDrawerClose } = this;
+		const value = {
+			...this.state,
+			handleDrawerClose
+		};
+		const { classes } = this.props;
+
+		return (
+			<AppContext.Provider value={value}>
+				<AppContext.Consumer>
+					{({ handleDrawerClose }) => (
+						<div className={classes.root}>
+							<CssBaseline />
+							<AppBar
+								position="fixed"
+								className={classNames(classes.appBar, {
+									[classes.appBarShift]: this.state.open
+								})}
+							>
+								<Toolbar disableGutters={!this.state.open}>
+									<IconButton
+										color="inherit"
+										aria-label="Open drawer"
+										onClick={this.handleDrawerOpen}
+										className={classNames(classes.menuButton, {
+											[classes.hide]: this.state.open
+										})}
+									>
+										<MenuIcon />
+									</IconButton>
+									<Typography variant="h6" color="inherit" noWrap>
+										Twitter Web Client
+									</Typography>
+								</Toolbar>
+							</AppBar>
+							<Drawer
+								variant="permanent"
+								className={classNames(classes.drawer, {
+									[classes.drawerOpen]: this.state.open,
+									[classes.drawerClose]: !this.state.open
+								})}
+								classes={{
+									paper: classNames({
+										[classes.drawerOpen]: this.state.open,
+										[classes.drawerClose]: !this.state.open
+									})
+								}}
+								open={this.state.open}
+							>
+								<Menu {...{ handleDrawerClose }} />
+							</Drawer>
+							<main className={classes.content}>
+								<Grid container spacing={24}>
+									<Grid item xs={12}>
+										<Paper className={classes.paper}>
+											Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae qui perferendis voluptas. A ratione
+											ad ullam qui architecto facilis culpa nostrum. Cumque optio inventore soluta iure recusandae.
+											Consequuntur, aut dolores. Est veniam, illo vitae a temporibus saepe ullam! Maxime maiores totam
+											iste, rem odio eaque corporis illo. Mollitia, est in! Ullam nesciunt rerum dolore cumque, ipsam
+											corporis minima hic voluptate. Quam voluptatem unde repellendus sapiente, consequatur doloremque
+											ipsa ex rerum officia suscipit quod sunt maiores vel veritatis earum libero! Eius laudantium
+											reiciendis exercitationem eligendi, mollitia eveniet quia inventore expedita. Labore? Quisquam
+											repellendus omnis tempora nemo, rerum temporibus. Nesciunt excepturi saepe odio impedit fuga
+											placeat tempora soluta facere corporis odit accusantium sint, voluptatem pariatur inventore
+											laudantium sit. Cum recusandae eos temporibus? Odit ratione, laudantium architecto esse tenetur
+											maiores fugit cupiditate commodi, eos omnis similique facilis? Molestiae eaque doloremque id
+											quibusdam pariatur similique voluptas veniam, tempore culpa vero, velit placeat est alias? Totam
+											at voluptatibus fugiat perspiciatis quia repellat, nemo ab, minus unde ratione id recusandae
+											veritatis? Beatae ea quis dolorum amet earum nam, fugiat necessitatibus iure labore quasi possimus
+											nisi in. Vel, totam! Error eos voluptate pariatur quia unde quae dolorem suscipit consequuntur cum
+											necessitatibus, a maiores deleniti temporibus saepe porro ab reprehenderit eligendi eius,
+											accusantium enim sequi deserunt ullam iusto? Aliquam corrupti culpa, totam facilis voluptatem
+											blanditiis dignissimos laboriosam repellat voluptas dolores, ad cumque, quod pariatur. Earum
+											voluptatibus facilis, cumque animi qui aliquam deserunt voluptas impedit id, molestias sit iusto!
+											Molestiae voluptatem sequi quasi corrupti. Consequatur dolores laudantium rem asperiores qui?
+											Mollitia asperiores aspernatur eveniet ratione hic beatae quod vero officiis nihil! Atque velit
+											distinctio voluptatum, repellendus quo id quisquam? Veritatis, atque ipsa! Aspernatur obcaecati
+											velit ipsam neque autem ut, quod, molestiae aut nam ratione repellendus iusto cumque? Sed neque
+											recusandae vel autem commodi quas eum ipsum? Error, itaque voluptatibus?
+										</Paper>
+									</Grid>
+								</Grid>
+							</main>
+						</div>
+					)}
+				</AppContext.Consumer>
+			</AppContext.Provider>
+		);
+	}
 }
 
-export default App;
+App.propTypes = {
+	classes: PropTypes.object.isRequired,
+	theme: PropTypes.object.isRequired
+};
+
+export default withStyles(styles, { withTheme: true })(App);
---
+++ src/components/menu/Menu.jsx
@@ -0,0 +1,56 @@
+import React, { Component, Fragment } from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import IconButton from '@material-ui/core/IconButton';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
+
+const styles = theme => ({
+	toolbar: {
+		display: 'flex',
+		alignItems: 'center',
+		justifyContent: 'flex-end',
+		padding: '0 8px',
+		...theme.mixins.toolbar
+	},
+	content: {
+		flexGrow: 1,
+		padding: theme.spacing.unit * 3
+	}
+});
+
+class Menu extends Component {
+	menuAction = () => {
+		const { handleDrawerClose } = this.props;
+		handleDrawerClose();
+	};
+
+	render() {
+		const { classes, theme } = this.props;
+		return (
+			<Fragment>
+				<div className={classes.toolbar}>
+					<IconButton onClick={this.menuAction}>
+						{theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
+					</IconButton>
+				</div>
+				<Divider />
+				<List>
+					{['Send email', 'Drafts'].map((text, index) => (
+						<ListItem button key={text}>
+							<ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+							<ListItemText primary={text} />
+						</ListItem>
+					))}
+				</List>
+			</Fragment>
+		);
+	}
+}
+export default withStyles(styles, { withTheme: true })(Menu);
